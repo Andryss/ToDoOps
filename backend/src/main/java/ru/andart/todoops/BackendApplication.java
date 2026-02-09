418ea6026729a62c3290e056baa1a28b977c19b5
Andryss <andr.krivosheev@gmail.com>
2026-02-09 22:32:58 +0300

    add backend checkstyle

--- backend/src/main/java/ru/andart/todoops/BackendApplication.java
+++ backend/src/main/java/ru/andart/todoops/BackendApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    /**
+     * Application entry point.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
